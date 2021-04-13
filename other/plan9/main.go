760a2fddd67eaf2c7af7a456c7989b7defec1bfa
waterloocode <330316259@qq.com>
2021-04-13 14:17:48 +0800

    feature: go 的值传递

--- other/plan9/main.go
+++ other/plan9/main.go
@@ -16,4 +16,17 @@ func main() {
 	defer func() {
 		fmt.Println("test3")
 	}()
+
+	s := []int{2, 2, 2}
+	fmt.Printf("out addr is %p", &s)
+	f(s)
+	fmt.Println(s)
+}
+
+func f(s []int) {
+	fmt.Printf("func addr is %p", &s)
+	for i, i2 := range s {
+		fmt.Println(i, i2)
+		s[i] += 2
+	}
 }
